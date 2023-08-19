537a32e23bd0945eb8e0a212d6ac5da8d717aed7
sarathrajan922 <sarathrajan922@gmail.com>
2023-08-19 10:21:12 +0530

    bubble sort 1st problem

--- BubbleSort/Bubblesort.js
+++ BubbleSort/Bubblesort.js
@@ -1,1 +1,20 @@
-console.log('bubble sort')
+let array = [20,1,66,34,8,3]
+console.log(array)
+const sort = (arr)=> {
+    let n= arr.length-1;
+    let swapped;
+    do{
+        swapped = false;
+        for(let i=0; i<n; i++){
+            if(arr[i]>arr[i+1]){
+                [arr[i],arr[i+1]]=[arr[i+1],arr[i]]
+                swapped = true
+            }
+        }
+        n--
+    }while(swapped)
+
+}
+
+sort(array)
+console.log(array)

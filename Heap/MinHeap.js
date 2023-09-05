f2d76d125a9a178dd30270a87f28af914ab3da48
sarathrajan922 <sarathrajan922@gmail.com>
2023-09-05 18:36:05 +0530

    min heap delete

--- Heap/MinHeap.js
+++ Heap/MinHeap.js
@@ -17,6 +17,33 @@ class Heap{
             }
         }
     }
+
+
+    delete(){
+        this.Heap[0] = this.Heap[this.Heap.length -1]
+        this.Heap.length--;
+        let i =0 
+        while(true){
+            let child1 = this.Heap[2*i+1]
+            let child2 = this.Heap[2*i+2]
+
+            if(child1 < child2){
+                if(child1 < this.Heap[i]){
+                    [this.Heap[i],this.Heap[2*i+1]]=[this.Heap[2*i+1],this.Heap[i]]
+                    i= 2*i+1
+                }else{
+                    return
+                }
+            }else{
+                if(child2 < this.Heap[i]){
+                    [this.Heap[i],this.Heap[2*i+2]]=[this.Heap[2*i+2],this.Heap[i]]
+                    i= 2*i+2
+                }else{
+                    return 
+                }
+            }
+        }
+    }
 }
 
 const H = new Heap()
@@ -30,4 +57,6 @@ H.insertValue(15)
 H.insertValue(16)
 H.insertValue(17)
 
+console.log(H.Heap)
+H.delete()
 console.log(H.Heap)

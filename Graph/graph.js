dbe510a0c48053ffc7f098934448345da0783eb3
sarathrajan922 <sarathrajan922@gmail.com>
2023-09-06 22:55:11 +0530

    Graph removeVetex

--- Graph/graph.js
+++ Graph/graph.js
@@ -22,6 +22,19 @@ class Graph{
             this.adjList[vertex2].add(vertex1)
         }
     }
+
+    removeVertex(vertex){
+        if(!this.adjList[vertex]) return
+        for(let vertices of this.adjList[vertex]){
+            this.removeEdge(vertex,vertices)
+        }
+        delete this.adjList[vertex]
+    }
+
+    removeEdge(vertex1,vertex2){
+        this.adjList[vertex1].delete(vertex2)
+        this.adjList[vertex2].delete(vertex1)
+    }
 }
 
 const G = new Graph();
@@ -34,5 +47,11 @@ G.addVertex(89)
 G.addEdge(20,30,false)
 G.addEdge(30,49,true)
 G.addEdge(89,20,true)
+
+
+
+console.log(G.adjList)
+
+G.removeVertex(30)
 
 console.log(G.adjList)
